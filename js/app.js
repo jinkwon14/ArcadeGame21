cd02a47c803164136fd69973ad431780b45329ce
JinK <jinkwon0425@gmail.com>
2016-11-01 16:20:15 -0400

    define player class and set variables

--- js/app.js
+++ js/app.js
@@ -24,12 +24,24 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.update = function() {
+
+  };
+  this.update = function() {
+
+  };
+  this.render = function() {
+
+  };
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 // Place the player object in a variable called player
-
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -27,6 +27,7 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
+    canvas.style = "border:1px solid #000000;";
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
