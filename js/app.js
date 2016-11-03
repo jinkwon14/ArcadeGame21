1eeb5644a8059d8162b53dc2b812335b29d9eccf
JinK <jinkwon0425@gmail.com>
2016-11-03 13:42:42 -0400

    refactor and beautify

--- js/app.js
+++ js/app.js
@@ -1,101 +1,121 @@
-// Enemies our player must avoid
+// ------- ENEMY BEGINS HERE
 var Enemy = function() {
     // load images
     this.sprite = 'images/enemy-bug.png';
 
-    // sets the initial position
+    // set initial position
     this.enemy_random_spawn();
 
-    // Set the enemy Speed.
+    // set enemy speed
     var rand_speed = rand_gen(4)
-    this.speed = 50*(1+rand_speed);
+    this.speed = 50 * (1 + rand_speed);
 };
 
-// *** Helper function that generates number from 0 to num-1
-var rand_gen = function(num) {
-  var pos = Math.random();
-  pos = Math.floor(pos/(1/num));
-  return pos;
-};
+var board_edge_right = 500;
+var board_edge_left = -5;
+var board_edge_top = 0;
+var board_edge_bottom = 375;
 
-// *** Helper function that generates random initial position of bugs
-Enemy.prototype.enemy_random_spawn = function() {
-  var rand_pos_x = rand_gen(3);
-  this.x = -100*(rand_pos_x+1); //+1 added to avoid spawning from 0th block
-  // starts at different "negative" x position to show the effect of coming
-  // in to the board at different time points.
-  var rand_pos_y = rand_gen(3);
-  this.y = 60 + rand_pos_y*80;
-  // this.y = 60;
-  // this.y = 140;
-  // this.y = 210;
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    if (this.x > 500) {
-      this.enemy_random_spawn();
+    var player_right_edge = player.x + 50;
+    var player_left_edge = player.x - 50;
+    var player_up_edge = player.y + 50;
+    var player_down_edge = player.y - 50;
+
+    // kills and respawns an enemy if it reaches the edge
+    if (this.x > board_edge_right) {
+        this.enemy_random_spawn();
     } else {
-      this.x = this.x +this.speed*dt;
+        this.x += this.speed * dt;
     }
 
-    // Handles collision with the player
-    if (this.y > player.y - 50 && this.y < player.y + 50 &&
-      this.x > player.x - 50 && this.x < player.x + 50 ) {
-      player.y = 370;
-      // TODO: relevant action needs to follow resulting from collision
+    // handles collision with the player
+    if (this.y > player_down_edge && this.y < player_up_edge &&
+        this.x > player_left_edge && this.x < player_right_edge) {
+        player.initial_pos();
+        // TODO: add more features resulting from collision
+        // collision();
     }
 };
 
-// Draw the enemy on the screen, required method for game
+// helper_fun 1: generates 'num' number of integers from 0
+var rand_gen = function(num) {
+    var pos = Math.random();
+    pos = Math.floor(pos / (1 / num));
+    return pos;
+};
+
+//  helper_fun 2: generates random initial position of enemie
+Enemy.prototype.enemy_random_spawn = function() {
+    var rand_pos_x = rand_gen(3);
+    this.x = -100 * (rand_pos_x + 1); //+1 to ensure spawning outside board
+    var rand_pos_y = rand_gen(3);
+    this.y = 60 + rand_pos_y * 80;
+    // from trial and error, valid y positions are 60, 140, 210
+};
+
 Enemy.prototype.render = function() {
-    // this.update(2);
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// ------- PLAYER BEGINS HERE
 var Player = function() {
-  this.sprite = 'images/char-boy.png';
-  // Set the initial position.
-  this.x = 200;
-  this.y = 370;
+    // load images
+    this.sprite = 'images/char-boy.png';
+
+    // set initial position
+    this.initial_pos();
 };
 
-Player.prototype.update = function(dt) {
-  // TODO: The update method for the Player (can be similar to the one for the Enemy)
-
+// ???? not sure if I am using this function right.
+// Why not just put this.handleInput(); inside #render and ignore this one? for consistency?
+Player.prototype.update = function() {
+    this.handleInput();
 };
 
 Player.prototype.render = function() {
-  this.handleInput();
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.update();
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(allowedKeys) {
-
-  if (allowedKeys === 'left') {
-    this.x - 100 > -5 ? this.x -= 100 : this.nono();
-  } else if (allowedKeys === 'right') {
-    this.x + 100 < 405 ? this.x += 100 : this.nono();
-  } else if (allowedKeys === 'up') {
-    this.y - 80 > 0 ? this.y -= 80 : this.nono();
-  } else if (allowedKeys === 'down') {
-    this.y + 80 < 375 ? this.y += 80 : this.nono();
-  } else {
-    this.y = this.y;
-  }
-
+    if (allowedKeys === 'left') {
+        this.x - 100 > board_edge_left ? this.x -= 100 : this.nono();
+    } else if (allowedKeys === 'right') {
+        this.x + 100 < board_edge_right ? this.x += 100 : this.nono();
+    } else if (allowedKeys === 'up') {
+        this.y - 80 > board_edge_top ? this.y -= 80 : this.win();
+    } else if (allowedKeys === 'down') {
+        this.y + 80 < board_edge_bottom ? this.y += 80 : this.nono();
+    } else {
+        this.y = this.y;
+    }
 };
 
-// TODO: Write a helper function that "nono"
+// player initial position
+Player.prototype.initial_pos = function() {
+    this.x = 200;
+    this.y = 370;
+};
+
+// TODO: notify invalid moves by either shacking or flashing
 Player.prototype.nono = function() {
 
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
-// Place the player object in a variable called player
+// resulting outcomes when the player wins (reaches the water)
+Player.prototype.win = function() {
+    // add an enemy to add difficulty
+    allEnemies.push(new Enemy());
+
+    // player go return back to initial position
+    this.initial_pos();
+    
+    // TODO: add more rewarding award for the winner if player accomplishes the goal set number of times
+};
+
+// initiate player and enemies
+var allEnemies = [new Enemy(), new Enemy()];
 var player = new Player();
 
 
